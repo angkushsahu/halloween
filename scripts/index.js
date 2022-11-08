05de935ebbeb3548601a838bc36c589e0d0c7745
Angkush <angkysahu2502@gmail.com>
2022-11-08 17:47:05 +0530

    added on scroll animations

--- scripts/index.js
+++ scripts/index.js
@@ -58,3 +58,18 @@ const scrollUpAction = () => {
 };
 
 window.addEventListener("scroll", scrollUpAction);
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "60px",
+    duration: 2500,
+    delay: 200,
+});
+
+sr.reveal(".home__data, .footer__content, .footer__logo, .footer__description");
+sr.reveal(".home__tree-1", { origin: "left", delay: 800 });
+sr.reveal(".home__tree-2", { origin: "right", delay: 800 });
+sr.reveal(".home__img", { delay: 800 });
+sr.reveal(".category__card, .items__card", { interval: 100 });
+sr.reveal(".about__img, .about__data, .footer__tree-2", { origin: "left" });
+sr.reveal(".party__images, .party__data, .footer__tree-1", { origin: "right" });
